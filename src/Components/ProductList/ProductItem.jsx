adf1a48bdc7ccf475a823a61e4b40df35b766337
Okoko Godwin-Jnr Orokpo <godwinjnrokokoorokpo@gmail.com>
2024-10-05 02:53:53 +0100

    add dynamic styling of list of available and unavailable product list

--- src/Components/ProductList/ProductItem.jsx
+++ src/Components/ProductList/ProductItem.jsx
@@ -3,7 +3,7 @@ import './ProductItem.css'
 function ProductItem ({product}){
     return (
         <>
-            <li className="item-list list-group-item ">
+            <li className="item-list list-group-item " style={{backgroundColor:product.isAvailable?'white':'#dedede'}}>
             <div className="list-container media align-items-lg-center flex-column flex-lg-row p-1 d-flex">
                 <div className="media-body order-2 order-lg-1">
                     <h5 className="mt-0 font-weight-bold mb-2">{product.Name}</h5>
